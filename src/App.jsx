a547c57b87051c1df78d5ed64c546a0ff711a9ae
João Pedro <joaopedropalomaresjpjp@gmail.com>
2024-03-06 15:35:10 -0300

    Criando a classe Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
 import './App.css'
-import Navegacao from './components/Navegacao/Navegacao'
-import Atracao from './components/Atracao/Atracao'
+import Home from './pages/Home/Home'
 
 function App() {
-  const componentes = [];
-  for(let i = 0; i <= 2; i++){
-    componentes.push(<Atracao key={i} />)
-  }
-
   return (
     <>
-      <Navegacao></Navegacao>
-      <div className="ctn-atracoes">
-        {componentes}
-      </div>
+      <Home />
     </>
   )
 }
