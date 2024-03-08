4798436c84cbf3f6a04a7b94f5d0279060e7cac5
João Pedro <joaopedropalomaresjpjp@gmail.com>
2024-03-08 10:39:15 -0300

    Criando as páginas Depoimentos, Fotos e Login

---
+++ src/pages/Fotos/Fotos.jsx
@@ -0,0 +1,13 @@
+import './Fotos.css'
+import Navegacao from '../../components/Navegacao/Navegacao'
+
+function Fotos () {
+    return (
+        <>
+            <Navegacao></Navegacao>
+            <p style={{color: 'black'}}>Nada aqui ainda</p>
+        </>
+    )
+}
+
+export default Fotos;
